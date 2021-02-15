330803440bfb55f62c5ae6a6710c501f81eeb04f
Mohamed Elawadi <mohamed@elawadi.net>
2021-02-15 12:04:54 +0100

    http 1.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Protocol Implementations
+
+[HTTP/1.1](./HTTP1.1.ipynb)
---
+++ utils.py
@@ -0,0 +1,69 @@
+import socket
+
+class ReaderWrapper:
+    def __init__(self, initial_value=b''):
+        self.buffer = initial_value
+        self.done = False
+        
+    def connect(self):
+        self.sock.connect((self.host, self.port))
+
+    def close(self):
+        self.sock.close()
+        
+    def send(self, data):
+        self.sock.sendall(data)
+        
+    def _read_to_buffer(self):
+        if not self.done:
+            buffer = self._read()
+            if buffer == b'':
+                self.done = True
+                return False
+            self.buffer += buffer
+            return True
+        else:
+            raise RuntimeError("socket closed can't read")
+
+    def peek_once(self):
+        return self.buffer
+
+    def read_once(self):
+        self._read_to_buffer()
+        out, self.buffer = self.buffer, b''
+        return out
+
+    def read_line(self):
+        return self.read_until(b'\r\n')
+
+    def read_until(self, delim):
+        while delim not in self.buffer:
+            self._read_to_buffer()
+        cut = self.buffer.index(delim) + len(delim)
+        out, self.buffer = self.buffer[:cut], self.buffer[cut:]
+        return out
+    
+    def read_len(self, l):
+        while len(self.buffer) < l:
+            self._read_to_buffer()
+        out, self.buffer = self.buffer[:l], self.buffer[l:]
+        return out
+
+    def read_all(self):
+        while self._read_to_buffer():
+            pass
+        out, self.buffer = self.buffer, b''
+        return out
+        
+class SocketReader(ReaderWrapper):
+    def __init__(self, host, port, type='tcp', initial_value=b''):
+        self.host = host
+        self.port = port
+        t = socket.SOCK_DGRAM if type == 'udp' else socket.SOCK_STREAM
+        self.sock = socket.socket(family=socket.AF_INET, type=t)
+        super().__init__()
+
+    def _read(self):
+        return self.sock.recv(4096)
+
+
